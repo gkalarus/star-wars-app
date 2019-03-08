3573ba8dad3e70190592e79aca9e8c623dce51f5
Grzegorz <kalarus.g@gmail.com>
2019-03-08 16:38:28 +0100

    Updates

---
+++ precache-manifest.d00f9a6b54da774b300194782dee844e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c09a29d8e14d1be7475",
+    "url": "/star-wars-app/static/css/main.da1ea0a2.chunk.css"
+  },
+  {
+    "revision": "6c09a29d8e14d1be7475",
+    "url": "/star-wars-app/static/js/main.6c09a29d.chunk.js"
+  },
+  {
+    "revision": "582433d4bb77a8510f75",
+    "url": "/star-wars-app/static/js/runtime~main.582433d4.js"
+  },
+  {
+    "revision": "d0e3d789864f457601ff",
+    "url": "/star-wars-app/static/css/2.5ca9c1b1.chunk.css"
+  },
+  {
+    "revision": "d0e3d789864f457601ff",
+    "url": "/star-wars-app/static/js/2.d0e3d789.chunk.js"
+  },
+  {
+    "revision": "4d66597d8badf6a02257a0704fa622fa",
+    "url": "/star-wars-app/static/media/ARROWOPEN.4d66597d.svg"
+  },
+  {
+    "revision": "9f3a719add9d3b8ad4f8d2918acb5059",
+    "url": "/star-wars-app/static/media/ARROWCLOSE.9f3a719a.svg"
+  },
+  {
+    "revision": "8b6d494529d2a18c9a45450ed663dce9",
+    "url": "/star-wars-app/static/media/LOGO.8b6d4945.svg"
+  },
+  {
+    "revision": "4b823e924575020f741e171a79ab181d",
+    "url": "/star-wars-app/static/media/deletePlanet.4b823e92.svg"
+  },
+  {
+    "revision": "3114a3b008896521924b608349d4efbc",
+    "url": "/star-wars-app/index.html"
+  }
+];
